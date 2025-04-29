9e3a4410064c4e6e8dba87c2e6235a3d5f3fb654
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-04-29 22:37:30 +0300

    added type definition. Changed the API url to fetch weather date for a week instead of current date only. Reset search input field to null after sccessful data fetch. Declared a variable 'content' to handle the displayed page based on search results, no error, error and welcome message. Added 'location to handleSearch function as the value of Search input field

--- app/page.tsx
+++ app/page.tsx
@@ -8,59 +8,95 @@ import Wind from "./components/wind";
 import Humidity from "./components/humidity";
 import Search from "./components/search";
 
+// data type definition
+interface ForecastData {
+  list: {
+    dt: number;
+    weather: { icon: string; description: string }[];
+    main: { temp: number };
+  }[];
+  city: {
+    id: number;
+    name: string;
+    coord: { lat: number; lon: number };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
 export default function Home() {
-  const [data, setData] = useState<any>({});
-  const [location, setLocation] = useState("Nairobi");
+  // states
+  const [data, setData] = useState<ForecastData | "">("");
+  const [location, setLocation] = useState("");
   const [error, setError] = useState("");
 
   const API = process.env.NEXT_PUBLIC_WEATHER_API;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API}&units=metric`;
 
-  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
+  // function to handle search input and fetch weather data of a city
+  const handleSearch = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=${API}`;
+
       try {
         const response = await fetch(url);
         if (!response.ok) throw new Error("Failed to fetch weather data.");
         const result = await response.json();
         setData(result);
         setError("");
+        setLocation(""); // Clears search input after a successful fetch
       } catch {
         setError("City not found");
-        setData({});
+        setData("");
       }
     }
   };
 
-  const fetchedDate = new Date(data.dt * 1000); // Convert the timestamp to a Date object
-  const day = fetchedDate.getDate(); // extract the day
-  const month = fetchedDate.toLocaleString("default", { month: "long" });
-
   let content;
+  // Show a welcome message if no search has been made and there is no error
   if (Object.keys(data).length === 0 && error === "") {
-    content = <div className="text-white">Welcome to Bree Weather App</div>;
-  } else if (error !== "") {
     content = (
-      <div>
+      <div className="text-white text-center font-semibold">
+        <h3 className="text-3xl">Welcome to Bree Weather App</h3>
+        <p className="text-xl">Enter a city to get weather forecast</p>
+      </div>
+    );
+  } else if (error !== "") {
+    // The error message displayed in case of an error in search input
+    content = (
+      <div className="text-center text-xl font-semibold">
         <p>City not found</p>
         <p>Please enter a valid city.</p>
       </div>
     );
   } else {
+    // The weather data of a searched city is displayed here
     content = (
-      <div>
-        <Today />
-        <Forecast />
+      <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1/2 md:w-1/3">
+          <Today data={data} />
+        </div>
+        <div className="w-full md:w-2/3">
+          <Forecast data={data} />
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen overflow-auto">
+    // <div className="bg-cover bg-gradient-to-r from-blue-700 to-blue-400 h-screen overflow-auto">
+    <div className="h-screen w-full bg-[url('/blue.jpg'))] bg-no-repeat bg-cover bg-center">
       <div className="bg-white/25 w-full rounded-lg flex flex-col h-fit p-4">
         <div className="flex flex-col md:flex-row justify-between items-center p-4">
-          <Search handleSearch={handleSearch} setLocation={setLocation} />
-          <h1 className="mb-8 md:mb-0 order-1 font-poppins text-white text-xl py-2 px-4 italic font-bold">
+          <Search
+            handleSearch={handleSearch}
+            location={location}
+            setLocation={setLocation}
+          />
+          <h1 className="mb-8 md:mb-0 order-1 text-white text-xl py-2 px-4 italic font-bold">
             Bree Weather App
           </h1>
         </div>
@@ -69,19 +105,6 @@ export default function Home() {
 
         {error && (
           <div className="text-red-200 text-center font-semibold">{error}</div>
-        )}
-
-        {data.main && (
-          <div className="text-white text-center text-lg mt-4 space-y-1">
-            <p>{`${day} ${month}`}</p>
-
-            <img
-              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-              alt={data.weather[0].description}
-              className="mx-auto"
-            />
-            <p> {data.main.temp} °C</p>
-          </div>
         )}
       </div>
     </div>
